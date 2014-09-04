8280e10260e52b90b1563b1d7d59f809bf981c6d
Gary Jackson <garyljackson@gmail.com>
2014-09-04 10:02:42 +1000

    Set up Web.Api with OWIN

---
+++ SecuredService.Web.Api/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using SecuredService.Web.Api;
+
+[assembly: OwinStartup(typeof (Startup))]
+
+namespace SecuredService.Web.Api
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+    }
+}
