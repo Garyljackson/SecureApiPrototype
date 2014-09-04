156d41a3e39db4e1a6c5c330fcae01bc2e1e4a54
Gary Jackson <garyljackson@gmail.com>
2014-09-04 10:10:34 +1000

    Added AuthContext

---
+++ SecuredService.Web.Api/Models/AuthContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace SecuredService.Web.Api.Models
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("AuthContext")
+        {
+        }
+    }
+}
