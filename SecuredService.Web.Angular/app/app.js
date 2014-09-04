8985aea52dbb03f31f34ddda7867708bb0f885e4
Gary Jackson <garyljackson@gmail.com>
2014-09-04 16:04:58 +1000

    Added angular home page route configuration.

--- SecuredService.Web.Angular/app/app.js
+++ SecuredService.Web.Angular/app/app.js
@@ -1,3 +1,16 @@
 ﻿'use strict';
 
-var app = angular.module('secureServiceApp', []);
+var app = angular.module('secureServiceApp', ['ngRoute']);
+
+app.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/home', {
+            templateUrl: '/app/views/home.html',
+            controller: 'homeController'
+        }).
+        otherwise({
+            redirectTo: '/home'
+        });
+  }]);
+
